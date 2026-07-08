1398db1e9ec9146e1bc335fb047ef27f20ce3345
TrakaWhy <chamikagihanedirisuriya@gmail.com>
2026-07-08 11:27:14 +0530

    update the date

--- componets/EngagementCountdown.tsx
+++ componets/EngagementCountdown.tsx
@@ -16,7 +16,7 @@ const beautifulFonts = localFont({
 })
 
 export const EngagementCountdown: React.FC = () => {
-    const TARGET_DATE = new Date('2026-7-30T18:00:00');
+    const TARGET_DATE = new Date('2026-07-30T18:00:00');
 
     const calculateTimeLeft = (): TimeLeft => {
         const now = new Date();
